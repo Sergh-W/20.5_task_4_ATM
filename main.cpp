f1b35621abaf80b836fcd6f096ff0cfe677fd567
Sergh-W <115610381+Sergh-W@users.noreply.github.com>
2025-03-20 21:51:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,35 @@ int comp (const void * a, const void * b)
     return ( *(int*)b - *(int*)a );
 }
 
+bool isDigit (const std::string &str)
+{
+    for (int i: str) {
+        if (std::isdigit(i)) continue;
+        else {
+            std::cerr << "Incorrect input!" << std::endl;
+            std::cerr << "Ending an ATM session.\nUntil next time." << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool checkAmount(const int &cash, const int &sum)
+{
+    if (cash % 100 != 0 || cash == 0) {
+        std::cerr << "Incorrect amount for withdrawal!" << std::endl;
+        std::cerr << "Ending an ATM session.\nUntil next time." << std::endl;
+        return false;
+    }
+
+    if (cash > sum) {
+        std::cerr << "Insufficient funds!" << std::endl;
+        std::cerr << "Ending an ATM session.\nUntil next time." << std::endl;
+        return false;
+    }
+    return true;
+}
+
 int main()
 {
     std::srand(std::time(nullptr));
@@ -22,47 +51,58 @@ int main()
 
         std::ifstream inData_ATM("..\\bank.bin", std::ios::binary);
 
-        inData_ATM.read ((char*) in_stock, sizeof(in_stock));
-
-        for (int i = 0; i < 1000; ++i)
+        if (!inData_ATM.is_open())
         {
-
-            if (in_stock[i] == 5000) ++count_5th;
-            if (in_stock[i] == 2000) ++count_2th;
-            if (in_stock[i] == 1000) ++count_1th;
-            if (in_stock[i] == 500) ++count_5h;
-            if (in_stock[i] == 200) ++count_2h;
-            if (in_stock[i] == 100) ++count_1h;
-
-            sum += in_stock[i];
-        }
-
-        inData_ATM.close();
-
-        if (sum == 0)
-        {
-            std::cerr << "There is no money in the ATM!!!" << std::endl;
-            std::cerr << "You can only top up by pressing '+', otherwise the ATM will shut down!!!" << std::endl;
+            std::cerr << "Error!!! Unable to read data from ATM." << std::endl;
         }
         else
+        {
+            inData_ATM.read((char *) in_stock, sizeof(in_stock));
+            for (int i = 0; i < 1000; ++i)
+            {
+                if (in_stock[i] == 5000) ++count_5th;
+                if (in_stock[i] == 2000) ++count_2th;
+                if (in_stock[i] == 1000) ++count_1th;
+                if (in_stock[i] == 500) ++count_5h;
+                if (in_stock[i] == 200) ++count_2h;
+                if (in_stock[i] == 100) ++count_1h;
+
+                sum += in_stock[i];
+            }
+            inData_ATM.close();
+        }
+        if (sum == 0)
+        {
+            std::cerr << "*******************************************************************************" << std::endl;
+            std::cerr << "There is no money in the ATM!!!" << std::endl;
+            std::cerr << "To deposit money into an ATM, press '+'" << std::endl;
+            std::cerr << "To exit and finish, press 'q' or another symbol" << std::endl;
+            std::cerr << "*******************************************************************************" << std::endl;
+            std::cin >> operation;
+            if (operation != '+') {
+                std::cout << "Ending an ATM session.\nUntil next time." << std::endl;
+                break;
+            }
+        }
+
+        if (sum > 0)
         {
             std::cout << std::endl;
             std::cout << "Hello. Available at ATM now:" << std::endl;
             std::cout << "Total amount = " << sum << std::endl;
             std::cout << "Banknotes: 5000 - " << count_5th << ", 2000 - " << count_2th << ", 1000 - " << count_1th <<
                       ", 500 - " << count_5h << ", 200 - " << count_2h << ", 100 - " << count_1h << std::endl;
-        }
-        std::cout << "*******************************************************************************" << std::endl;
-        std::cout << "Please select an operation to continue:" << std::endl;
-        std::cout << "To withdraw cash, press '-'" << std::endl;
-        std::cout << "To deposit money into an ATM, press '+'" << std::endl;
-        std::cout << "To exit and finish, press 'q' or another symbol" << std::endl;
-        std::cout << "*******************************************************************************" << std::endl;
-
-        std::cin >> operation;
-
-        if ((sum == 0 && operation != '+') || (operation != '+' && operation != '-')) {
-            break;
+            std::cout << "*******************************************************************************" << std::endl;
+            std::cout << "Please select an operation to continue:" << std::endl;
+            std::cout << "To withdraw cash, press '-'" << std::endl;
+            std::cout << "To deposit money into an ATM, press '+'" << std::endl;
+            std::cout << "To exit and finish, press 'q' or another symbol" << std::endl;
+            std::cout << "*******************************************************************************" << std::endl;
+            std::cin >> operation;
+            if (operation != '+' && operation != '-') {
+                std::cout << "Ending an ATM session.\nUntil next time." << std::endl;
+                break;
+            }
         }
 
         if (operation == '+')
@@ -76,76 +116,60 @@ int main()
             }
 
             qsort(in_stock, 1000, sizeof(int), comp);
-
             data_ATM.write((char*) in_stock, sizeof(in_stock));
-
             data_ATM.close();
         }
 
-        if (operation == '-')
-        {
+        if (operation == '-') {
             int cash = 0;
+            std::string enteredStr = "";
             std::cout << "What amount do you want to withdraw?" << std::endl;
-            std::cin >> cash;
+            std::cin >> enteredStr;
 
-            if (cash % 100 != 0 || cash <= 0) {
-                std::cerr << "Incorrect amount for withdrawal!" << std::endl;
-                break;
-            }
+            if (!isDigit(enteredStr)) break;
 
-            else if ((sum - cash) < 0) {
-                std::cerr << "Insufficient funds!" << std::endl;
-                break;
-            }
+            cash = stoi(enteredStr);
 
-            else
-            {
-                int count = 0;
-                while (cash > 0) {
-                    if (in_stock[count] == 5000 && cash >= 5000) {
-                        in_stock[count] = 0;
-                        cash -= 5000;
-                        ++count;
-                    }
-                    if (in_stock[count] == 2000 && cash >= 2000) {
-                        in_stock[count] = 0;
-                        cash -= 2000;
-                        ++count;
-                    }
-                    if (in_stock[count] == 1000 && cash >= 1000) {
-                        in_stock[count] = 0;
-                        cash -= 1000;
-                        ++count;
-                    }
-                    if (in_stock[count] == 500 && cash >= 500) {
-                        in_stock[count] = 0;
-                        cash -= 500;
-                        ++count;
-                    }
-                    if (in_stock[count] == 200 && cash >= 200) {
-                        in_stock[count] = 0;
-                        cash -= 200;
-                        ++count;
-                    }
-                    if (in_stock[count] == 100 && cash >= 100) {
-                        in_stock[count] = 0;
-                        cash -= 100;
-                        ++count;
-                    }
+            if (!checkAmount(cash, sum)) break;
+
+            sum -= cash;
+
+            for (int count = 0; cash != 0; ++count) {
+                if (in_stock[count] == 5000 && cash >= 5000) {
+                    in_stock[count] = 0;
+                    cash -= 5000;
                 }
-
-                std::cout << "Money has been issued." << std::endl;
-
-                std::ofstream data_ATM("..\\bank.bin", std::ios::binary);
-
-                qsort(in_stock, 1000, sizeof(int), comp);
-
-                data_ATM.write((char*) in_stock, sizeof(in_stock));
-
-                data_ATM.close();
+                if (in_stock[count] == 2000 && cash >= 2000) {
+                    in_stock[count] = 0;
+                    cash -= 2000;
+                }
+                if (in_stock[count] == 1000 && cash >= 1000) {
+                    in_stock[count] = 0;
+                    cash -= 1000;
+                }
+                if (in_stock[count] == 500 && cash >= 500) {
+                    in_stock[count] = 0;
+                    cash -= 500;
+                }
+                if (in_stock[count] == 200 && cash >= 200) {
+                    in_stock[count] = 0;
+                    cash -= 200;
+                }
+                if (in_stock[count] == 100 && cash >= 100) {
+                    in_stock[count] = 0;
+                    cash -= 100;
+                }
             }
+
+            std::cout << "Money has been issued." << std::endl;
+
+            std::ofstream data_ATM("..\\bank.bin", std::ios::binary);
+            qsort(in_stock, 1000, sizeof(int), comp);
+            data_ATM.write((char *) in_stock, sizeof(in_stock));
+            data_ATM.close();
+
+            if (sum == 0) break;
         }
     }
-
     return 0;
 }
